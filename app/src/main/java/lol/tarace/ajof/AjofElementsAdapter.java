4335c070cfedaa12198971065799edeef0bb7c48
Mickael Goubin <micky0304@gmail.com>
2016-03-13 17:08:00 +0100

    Display a list of ajof sounds from raw files

---
+++ app/src/main/java/lol/tarace/ajof/AjofElementsAdapter.java
@@ -0,0 +1,61 @@
+package lol.tarace.ajof;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by magicmicky on 16/02/16.
+ */
+
+public class AjofElementsAdapter extends RecyclerView.Adapter<AjofElementsAdapter.ViewHolder> {
+    private List<String> mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ViewHolder(View v) {
+            super(v);
+            mTextView = (TextView) v.findViewById(R.id.info_text);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public AjofElementsAdapter(List<String> myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public AjofElementsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.ajof_element, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset.get(position));
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
--- app/src/main/java/lol/tarace/ajof/MainActivity.java
+++ app/src/main/java/lol/tarace/ajof/MainActivity.java
@@ -4,19 +4,44 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Retrieve assets from
+        Field[] fields=R.raw.class.getFields();
+        final List<String> myDataset = new ArrayList<String>();
+        for (Field field : fields) {
+            Log.i("Raw Asset: ", field.getName());
+            myDataset.add(field.getName());
+            int resID = this.getResources().getIdentifier(field.getName(), "raw", this.getPackageName());
+        }
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RecyclerView rv = (RecyclerView) findViewById(R.id.RV_ajof_elements);
+        rv.setHasFixedSize(true);
+        RecyclerView.LayoutManager lm = new LinearLayoutManager(this);
+        rv.setLayoutManager(lm);
+
+        AjofElementsAdapter adapter = new AjofElementsAdapter(myDataset);
+        rv.setAdapter(adapter);
 
     }
 
